6493cae4e5ba8f0f8cea6f5e193ae268c3b9e8aa
Gene Liverman <gene@technicalissues.us>
2017-01-25 10:58:42 -0500

    added cachedir setting

--- site/profile/manifests/foreman_aio.pp
+++ site/profile/manifests/foreman_aio.pp
@@ -45,6 +45,7 @@ class profile::foreman_aio {
 
   class { 'r10k':
     provider          => 'puppet_gem',
+    cachedir          => '/opt/puppetlabs/puppet/cache/r10k',
     configfile        => '/etc/puppetlabs/r10k/r10k.yaml',
     manage_modulepath => false,
     sources           => {
